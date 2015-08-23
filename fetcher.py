fee766f9dd6edfbb24e300063c3133f01b71af97
PegasusPrime <riverrat87@gmail.com>
2015-08-23 17:23:34 -0400

    Update fetcher

--- fetcher.py
+++ fetcher.py
@@ -22,6 +22,13 @@ class Game(Base):
     def __repr__(self):
         return "<Game(id='%s', initial_price='%s', final_price='%s')>" % (self.id, self.init_price, self.final_price)
 
+def build_list():
+    game_list = dict()
+    URL = "http://api.steampowered.com/ISteamApps/GetAppList/v2"
+    response = requests.get(URL)
+    game_list = json.loads(response.text)
+    return game_list
+
 def fetchdump(appids):
     #TODO: Make sure we were fed a list of strings, not list of ints
     Session = sessionmaker(bind=engine)
@@ -39,13 +46,21 @@ def fetchdump(appids):
                 final_price = data[game]["data"]["price_overview"]["final"]
                 game_obj = Game(id=game, init_price=init_price, final_price=final_price)
                 session.add(game_obj)
+        else:
+            print("ID %s is false" % game)
     try:
         session.commit()
     except IntegrityError as err:
-        print("Error updateing DB! %s" % err)
+        print("Error updating DB! %s" % err)
 
 def main():
-    appids=["383","440","730","31820","8190"]
+    master_list = build_list()
+    #TEST: first 20 in list, earliest ID's. There has got to be a better way to do this!
+    apps = ""
+    for app in master_list["applist"]["apps"][:20]:
+        junk = " " + str(app["appid"])
+        apps += junk
+    appids = apps.split(' ')
     Base.metadata.create_all(engine)
     fetchdump(appids)
 
